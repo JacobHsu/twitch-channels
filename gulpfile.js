cfb09c704f1d0085aa6e05a2346c8f570761b122
jacobhsu <jacob.hsu.tw@gmail.com>
2018-04-30 22:42:00 +0800

    gulp-inline

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var inline = require('gulp-inline');
+
+gulp.task('inline',function(){
+	return gulp.src('./index.html')
+	.pipe(inline({
+		base:'./'
+	}))
+	.pipe(gulp.dest('dist/'));
+});
+
+gulp.task('default',['inline']);
